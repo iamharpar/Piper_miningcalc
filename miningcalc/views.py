980e101b3227e1a378aa2ae26fb082407311eb21
iamharpar <iamharpar1@gmail.com>
2019-11-01 18:02:24 +0530

    Minor Template optimisations

--- miningcalc/views.py
+++ miningcalc/views.py
@@ -8,6 +8,13 @@ class miningcalc(View):
     template_name = 'index.html'
     net_profit = 0
     def get(self,request,*args,**kwargs):
+        print(request.GET)
+        # self.hash_rate = float(request.GET.get('hashRate'))
+        # self.consumed_power = float(request.GET.get('power'))
+        # self.costKWh = float(request.GET.get('cost'))
+        # self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
+        # self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+        # print("NET PROFIT: {}".format(self.net_profit))
         context = {
             'net_profit': self.net_profit
         }
