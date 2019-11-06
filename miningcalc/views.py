0fb5071a12a7bb8fc0c68227c05da8e7b6aecb66
iamharpar <iamharpar1@gmail.com>
2019-11-06 12:07:08 +0530

    Added AJAX polling and improved templates

--- miningcalc/views.py
+++ miningcalc/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
 import cryptocompare as cryptcomp
 
 # Create your views here.
 class miningcalc(View):
     queryset = ''
-    template_name = 'index.html'
+    hash_rate = 12
+    consumed_power = 10
+    costKWh = 8
+    template_name = 'test.html'
     net_profit = 0
     def get(self,request,*args,**kwargs):
         print(request.GET)
-        # self.hash_rate = float(request.GET.get('hashRate'))
-        # self.consumed_power = float(request.GET.get('power'))
-        # self.costKWh = float(request.GET.get('cost'))
-        # self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
-        # self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
-        # print("NET PROFIT: {}".format(self.net_profit))
+        requests = request.GET
+        if request.GET:
+            print("is not empty")
+            self.hash_rate = float(request.GET.get('hashRate'))
+            self.consumed_power = float(request.GET.get('power'))
+            self.costKWh = float(request.GET.get('cost'))
+            self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
+            self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+            print("NET PROFIT: {} with hashRate after AJAX: {}".format(self.net_profit,self.hash_rate))
+            return JsonResponse({
+                "net_profit": self.net_profit
+            })
+        else:
+            self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
+            self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+            print("NET PROFIT: {} with hasRate befor AJAX: {}".format(self.net_profit,self.hash_rate))
+            print("is empty")
         context = {
-            'net_profit': self.net_profit
+            'net_profit': self.net_profit,
+            'hashRate': self.hash_rate,
+            'power': self.consumed_power,
+            'cost': self.costKWh
         }
         return render(request,self.template_name, {'context':context})
     def post(self,request,*args,**kwargs):
@@ -27,6 +45,9 @@ class miningcalc(View):
         self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
         print("NET PROFIT: {}".format(self.net_profit))
         context = {
-            'net_profit': self.net_profit
+            'net_profit': self.net_profit,
+            'hashRate': self.hash_rate,
+            'power': self.consumed_power,
+            'cost': self.costKWh
         }
         return render(request,self.template_name, {'context':context})
