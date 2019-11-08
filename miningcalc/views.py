275857e9682e7fbff831422a41b0fa01d6f61eef
iamharpar <iamharpar1@gmail.com>
2019-11-08 09:07:24 +0530

    Added other effecting factors

--- miningcalc/views.py
+++ miningcalc/views.py
@@ -9,6 +9,9 @@ class miningcalc(View):
     hash_rate = 12
     consumed_power = 10
     costKWh = 8
+    block_reward = 12.5
+    block_time = 600
+    network_hash_rate = 1.8410989266292908E9
     template_name = 'index.html'
     net_profit = 0
     def get(self,request,*args,**kwargs):
@@ -20,14 +23,14 @@ class miningcalc(View):
             self.consumed_power = float(request.GET.get('power'))
             self.costKWh = float(request.GET.get('cost'))
             self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
-            self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+            self.net_profit = ((self.hash_rate / self.network_hash_rate) * (self.block_reward * self.block_time) * self.coin_price) - (self.costKWh * (self.consumed_power / 1000))
             print("NET PROFIT: {} with hashRate after AJAX: {}".format(self.net_profit,self.hash_rate))
             return JsonResponse({
                 "net_profit": self.net_profit
             })
         else:
             self.coin_price = cryptcomp.get_price('BTC',curr='USD')['BTC']['USD']
-            self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+            self.net_profit = ((self.hash_rate / self.network_hash_rate) * (self.block_reward * self.block_time) * self.coin_price) - (self.costKWh * (self.consumed_power / 1000))
             print("NET PROFIT: {} with hasRate befor AJAX: {}".format(self.net_profit,self.hash_rate))
             print("is empty")
         context = {
@@ -42,7 +45,7 @@ class miningcalc(View):
         self.consumed_power = float(request.POST.get('power'))
         self.costKWh = float(request.POST.get('cost'))
         self.coin_price = cryptcomp.get_price('BTC',curr="USD")['BTC']['USD']
-        self.net_profit = self.hash_rate * self.coin_price - self.costKWh * self.consumed_power
+        self.net_profit = ((self.hash_rate / self.network_hash_rate) * (self.block_reward * self.block_time) * self.coin_price) - (self.costKWh * (self.consumed_power / 1000))
         print("NET PROFIT: {}".format(self.net_profit))
         context = {
             'net_profit': self.net_profit,
